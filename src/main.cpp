c76ddd07535dc3a11401a07b6269edc335171e23
HexSeal <maxfin13@gmail.com>
2021-07-24 16:00:19 -0400

    [add] Basic CLI implementation done

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <boost/program_options.hpp>
+#include <iterator>
 
 #include "parse.h"
 #include "compiling.h"
@@ -8,11 +9,42 @@
 using namespace std;
 namespace po = boost::program_options;
 
+
+
 int main(int argc, char *argv[])
 {
     // cout << "argc == " << argc << '\n';
-    parse();
 
-    return EXIT_SUCCESS; 
+    try {
+        // Add options and their descriptions
+        po::options_description desc("Options");
+        desc.add_options()
+            ("help,h", "List of available commands")
+            ("version", "Current version")
+            ("build", "Build and run the program");
+
+        // Get the input from the command line and store it in a variable map
+        po::variables_map vm;
+        po::store(parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+
+        // Output based on command
+        if (vm.count("help"))
+            std::cout << desc << '\n';
+        else if (vm.count("version"))
+            // std::cout << "Current version: " << vm["age"].as<float>() << '\n';
+            std::cout << "Current version: 0.1" << '\n';
+        else if (vm.count("build"))
+            std::cout << "And this is where I would put my build functions..." << '\n';
+    }
+
+    catch(exception& e) {
+        cerr << "error: " << e.what() << "\n";
+        return 1;
+    }
+    catch(...) {
+        cerr << "Exception of unknown type!\n";
+    }
+
+    return 0;
 }
-
