1e8c38d1b1ee387eb584052702c257804b1a8f46
Seth Itow <11025699+sethitow@users.noreply.github.com>
2021-08-19 16:06:53 -0700

    reformat source files (#7)

--- src/parse.cpp
+++ src/parse.cpp
@@ -1,18 +1,21 @@
-#include <iostream>
-#include <fstream>
 #include "parse.hpp"
 
-int parse(std::string filename) {
+#include <fstream>
+#include <iostream>
+
+int parse(std::string filename)
+{
     std::cout << "Parsing " << filename << std::endl;
 
     std::string oneLine;
     std::ifstream inFile(filename);
-    
-    while (inFile) {
+
+    while (inFile)
+    {
         getline(inFile, oneLine);
         std::cout << oneLine << std::endl;
     }
     inFile.close();
-    
+
     return EXIT_SUCCESS;
 }
