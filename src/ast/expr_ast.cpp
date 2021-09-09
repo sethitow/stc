908afc18be32006004d8373a9512a80e08a1aad9
Seth Itow <11025699+sethitow@users.noreply.github.com>
2021-09-09 16:36:33 -0700

    [add] Code generation branch, can't currently use the CLI

--- src/ast/expr_ast.cpp
+++ src/ast/expr_ast.cpp
@@ -1,13 +1,48 @@
 #include "expr_ast.hpp"
+#include <map>
+#include "llvm/IR/IRBuilder.h"
 
-llvm::Value *VariableExprAST::codegen()
-{
-    // TODO: implement code generation
+static std::unique_ptr<llvm::LLVMContext> TheContext;
+static std::unique_ptr<llvm::Module> TheModule;
+static std::unique_ptr<llvm::IRBuilder<>> Builder;
+static std::map<std::string, llvm::Value *> NamedValues;
+
+llvm::Value *LogErrorV(const char *Str) {
+    LogErrorV(Str);
     return nullptr;
+}
+
+llvm::Value *NumberExprAST::codegen() {
+    return llvm::ConstantInt::get(*TheContext, llvm::APInt(64, Val, true));
+}
+
+llvm::Value *VariableExprAST::codegen() {
+    llvm::Value *V = NamedValues[Name];
+    if (!V)
+        return LogErrorV("Unknown variable name");
+    return V;
 }
 
 llvm::Value *BinaryExprAST::codegen()
 {
-    // TODO: implement code generation
-    return nullptr;
+    llvm::Value *L = LHS->codegen();
+    llvm::Value *R = RHS->codegen();
+    if (!L || !R)
+        return nullptr;
+
+    switch (Op) {
+    case '+':
+        return Builder->CreateAdd(L, R, "addtmp", false, false);
+    case '-':
+        return Builder->CreateAdd(L, R, "subtmp", false, false);
+    case '*':
+        return Builder->CreateAdd(L, R, "multmp", false, false);
+    // case '<':
+    //     L = Builder->CreateFCmpULT(L, R, "cmptmp");
+    //     // Convert bool 0/1 to double 0.0 or 1.0
+    //     return Builder->CreateUIToFP(L, llvm::Type::getDoubleTy(TheContext),
+    //                                 "booltmp");
+    default:
+        return LogErrorV("invalid binary operator");
+    }
 }
